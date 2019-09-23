81d97327ce80c7df3191efa86e08914c5fa50312
Justin Carter <justin@misthos.io>
2019-09-23 08:06:21 +0200

    Revert "Introduce IdentifiedConnection"
    This reverts commit fcd5f4725c976c42cf92f9785ba61148ef014e95.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod server;
 use crate::error::Error;
 use actix::{Arbiter, System};
 use bisq::{constants::BaseCurrencyNetwork, message::*};
+use connection::ConnectionConfig;
 use env_logger;
 use listener::{Accept, Listener};
 use peers::Peers;
