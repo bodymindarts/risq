5b799d782c0b8c69175f2773b67f23ab7d20bdcf
Justin Carter <justin@misthos.io>
2019-10-13 08:44:09 +0200

    Use inner and empty modules in stats

--- src/lib.rs
+++ src/lib.rs
@@ -8,30 +8,9 @@ mod domain;
 mod error;
 mod p2p;
 mod prelude;
-#[cfg(feature = "stats")]
 mod stats;
 
 pub mod cli;
 
 #[macro_use]
 extern crate log;
-#[macro_use]
-extern crate futures;
-
-#[cfg(not(feature = "stats"))]
-mod stats {
-    use crate::prelude::*;
-    use actix_web::{Error, HttpResponse};
-
-    pub struct StatsLog;
-    pub struct Schema;
-    pub fn create_schema() -> Schema {
-        Schema
-    }
-    pub fn graphql() -> impl Future<Item = HttpResponse, Error = Error> {
-        future::ok(HttpResponse::Ok().finish())
-    }
-    pub fn graphiql() -> HttpResponse {
-        HttpResponse::Ok().finish()
-    }
-}
--- src/stats.rs
+++ src/stats.rs
@@ -1,71 +1,95 @@
-use crate::bisq::payload::TradeStatistics2;
-use crate::prelude::*;
-use actix_web::{web, Error, HttpResponse};
-use juniper::{
-    self, graphql_object,
-    http::{graphiql::graphiql_source, GraphQLRequest},
-    EmptyMutation, FieldResult, GraphQLInputObject, RootNode,
-};
-use juniper_from_schema::graphql_schema_from_file;
-use std::sync::Arc;
+mod inner {
+    use crate::bisq::payload::TradeStatistics2;
+    use crate::prelude::*;
+    use actix_web::{web, Error, HttpResponse};
+    use juniper::{
+        self, graphql_object,
+        http::{graphiql::graphiql_source, GraphQLRequest},
+        EmptyMutation, FieldResult, GraphQLInputObject, RootNode,
+    };
+    use juniper_from_schema::graphql_schema_from_file;
+    use std::sync::Arc;
 
-pub fn graphql(
-    schema: web::Data<Arc<Schema>>,
-    log: web::Data<StatsLog>,
-    request: web::Json<GraphQLRequest>,
-) -> impl Future<Item = HttpResponse, Error = Error> {
-    web::block(move || {
-        let res = request.execute(&schema, &log);
-        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
-    })
-    .map_err(Error::from)
-    .and_then(|user| {
-        Ok(HttpResponse::Ok()
-            .content_type("application/json")
-            .body(user))
-    })
-}
-pub fn graphiql() -> HttpResponse {
-    let html = graphiql_source("http://localhost:7477/graphql");
-    HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(html)
-}
+    pub fn graphql(
+        schema: web::Data<Arc<Schema>>,
+        log: web::Data<StatsLog>,
+        request: web::Json<GraphQLRequest>,
+    ) -> impl Future<Item = HttpResponse, Error = Error> {
+        web::block(move || {
+            let res = request.execute(&schema, &log);
+            Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
+        })
+        .map_err(Error::from)
+        .and_then(|user| {
+            Ok(HttpResponse::Ok()
+                .content_type("application/json")
+                .body(user))
+        })
+    }
+    pub fn graphiql() -> HttpResponse {
+        let html = graphiql_source("http://localhost:7477/graphql");
+        HttpResponse::Ok()
+            .content_type("text/html; charset=utf-8")
+            .body(html)
+    }
 
-graphql_schema_from_file!("src/api/stats_schema.graphql", context_type: StatsLog);
+    graphql_schema_from_file!("src/api/stats_schema.graphql", context_type: StatsLog);
 
-pub struct Query;
-impl QueryFields for Query {
-    fn field_hello_world(
-        &self,
-        executor: &juniper::Executor<'_, StatsLog>,
-        name: String,
-    ) -> juniper::FieldResult<String> {
-        Ok(format!("Hello, {}!", name))
+    pub struct Query;
+    impl QueryFields for Query {
+        fn field_hello_world(
+            &self,
+            executor: &juniper::Executor<'_, StatsLog>,
+            name: String,
+        ) -> juniper::FieldResult<String> {
+            Ok(format!("Hello, {}!", name))
+        }
+    }
+
+    type Mutation = EmptyMutation<StatsLog>;
+
+    pub fn create_schema() -> Schema {
+        Schema::new(Query {}, EmptyMutation::new())
+    }
+
+    type StatsLogInner = Arc<locks::RwLock<Vec<TradeStatistics2>>>;
+    #[derive(Clone)]
+    pub struct StatsLog(pub StatsLogInner);
+    impl juniper::Context for StatsLog {}
+
+    pub struct Stats {
+        statistics: StatsLogInner,
+    }
+    impl Stats {
+        pub fn start() -> (StatsLog, Addr<Self>) {
+            let statistics = Arc::new(locks::RwLock::new(Vec::new()));
+            (StatsLog(statistics.clone()), Self { statistics }.start())
+        }
+    }
+
+    impl Actor for Stats {
+        type Context = Context<Self>;
     }
 }
 
-type Mutation = EmptyMutation<StatsLog>;
+mod empty {
+    use crate::prelude::*;
+    use actix_web::{Error, HttpResponse};
 
-pub fn create_schema() -> Schema {
-    Schema::new(Query {}, EmptyMutation::new())
-}
-
-type StatsLogInner = Arc<locks::RwLock<Vec<TradeStatistics2>>>;
-#[derive(Clone)]
-pub struct StatsLog(pub StatsLogInner);
-impl juniper::Context for StatsLog {}
-
-pub struct Stats {
-    statistics: StatsLogInner,
-}
-impl Stats {
-    pub fn start() -> (StatsLog, Addr<Self>) {
-        let statistics = Arc::new(locks::RwLock::new(Vec::new()));
-        (StatsLog(statistics.clone()), Self { statistics }.start())
+    pub struct StatsLog;
+    pub struct Schema;
+    pub fn create_schema() -> Schema {
+        Schema
+    }
+    pub fn graphql() -> impl Future<Item = HttpResponse, Error = Error> {
+        future::ok(HttpResponse::Ok().finish())
+    }
+    pub fn graphiql() -> HttpResponse {
+        HttpResponse::Ok().finish()
     }
 }
 
-impl Actor for Stats {
-    type Context = Context<Self>;
-}
+#[cfg(not(feature = "stats"))]
+pub use empty::*;
+#[cfg(feature = "stats")]
+pub use inner::*;
