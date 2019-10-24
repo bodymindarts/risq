09c7e59e9fab61d486b915a940c22162f5c6ed2f
Justin Carter <justin@misthos.io>
2019-10-25 01:13:26 +0200

    Fix build-release.sh

--- ci/tasks/build-release.sh
+++ ci/tasks/build-release.sh
@@ -4,6 +4,7 @@ set -eu
 
 REPO=${REPO:-repo}
 BINARY=risq
+OUT=${OUT:-none}
 
 export CARGO_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../" >/dev/null && pwd )/cargo-home"
 
@@ -13,6 +14,6 @@ export CARGO_TARGET_DIR="../cargo-target-dir"
 
 make build-${TARGET}-release
 
-if [[ "${OUT}" != "" ]]; then
-mv ${CARGO_TARGET_DIR}/${TARGET}/release/${BINARY} ${RELEASE_ROOT}/artifacts/${BINARY}-${TARGET}
+if [[ "${OUT}" != "none" ]]; then
+  mv ${CARGO_TARGET_DIR}/${TARGET}/release/${BINARY} ${RELEASE_ROOT}/artifacts/${BINARY}-${TARGET}
 fi
