09c0b38e81ea158f0f1f1209906b78bf132db9a1
Justin Carter <justin@misthos.io>
2019-10-25 00:52:45 +0200

    Reduce github-release

--- ci/tasks/github-release.sh
+++ ci/tasks/github-release.sh
@@ -5,62 +5,19 @@ set -eu
 VERSION=$(cat version/number)
 BINARY=risq
 WORKSPACE="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../" >/dev/null && pwd )"
-REPO_ROOT="${WORKSPACE}/repo"
+REPO_ROOT="${WORKSPACE}/out-repo/git"
 RELEASE_ROOT="${WORKSPACE}/gh-release"
 RELEASE_NAME="risq release"
-REPO_OUT="${WORKSPACE}/out-repo"
-export CARGO_HOME="${WORKSPACE}/cargo-home"
-
-if [[ ! -f ${REPO_ROOT}/ci/release_notes.md ]]; then
-  echo >&2 "ci/release_notes.md not found.  Did you forget to write them?"
-  exit 1
-fi
-if [[ "$(cat ${REPO_ROOT}/ci/release_notes.md | wc -l | tr -d [:space:])" == "1" ]];then
-  echo >&2 "ci/release_notes.md only contains 1 line. Did you forget to write them?"
-  exit 1
-fi
-
-TARGET="x86_64-unknown-linux-gnu"
-rustup target add ${TARGET}
 
 pushd $REPO_ROOT
 
-cat <<EOF >new_change_log.md
-# [risq release v${VERSION}](https://github.com/bodymindarts/risq/releases/tag/v${VERSION})
-
-$(cat ci/release_notes.md)
-
-$(cat CHANGELOG.md)
-EOF
-mv new_change_log.md CHANGELOG.md
-
-sed -i'' "0,/version/{s/version.*/version = \"${VERSION}\"/}" Cargo.toml
-sed -i'' "/^name = \"risq/,/version/{s/version.*/version = \"${VERSION}\"/}" ./Cargo.lock
-mv ${REPO_ROOT}/ci/release_notes.md          ${RELEASE_ROOT}/notes.md
-echo "Empty - please add release notes here" > ${REPO_ROOT}/ci/release_notes.md
-
-# GIT!
-if [[ -z $(git config --global user.email) ]]; then
-  git config --global user.email "risqbot@misthos.io"
-fi
-if [[ -z $(git config --global user.name) ]]; then
-  git config --global user.name "CI Bot"
-fi
-
-(cd ${REPO_ROOT}
- git merge --no-edit ${BRANCH}
- git add -A
- git status
- git commit -m "Release v${VERSION}")
-
-export CARGO_TARGET_DIR="../cargo-target-dir"
-cargo build --features "all" --target ${TARGET} --release --locked
+mv ${REPO_ROOT}/../notes.md ${RELEASE_ROOT}/notes.md
 
 mkdir -p ${RELEASE_ROOT}/artifacts
-mv ${CARGO_TARGET_DIR}/${TARGET}/release/${BINARY} ${RELEASE_ROOT}/artifacts/${BINARY}-${TARGET}
+mv x86_64-unknown-linux-gnu/risq ${RELEASE_ROOT}/artifacts/${BINARY}-x86_64-unknown-linux-gnu
+mv arm-unknown-linux-gnueabihf/risq ${RELEASE_ROOT}/artifacts/${BINARY}-arm-unknown-linux-gnueabihf
 
 echo "v${VERSION}"                         > ${RELEASE_ROOT}/tag
 echo "${RELEASE_NAME} v${VERSION}"         > ${RELEASE_ROOT}/name
 
-# so that future steps in the pipeline can push our changes
-cp -a ${REPO_ROOT} ${REPO_OUT}/git
+exit 1
