7a1f7e5c3ea0653a38b528690f5b68e5a0a9780e
Justin Carter <justin@misthos.io>
2019-10-25 00:44:30 +0200

    More granular shipit tasks

--- ci/tasks/build-release.sh
+++ ci/tasks/build-release.sh
@@ -2,10 +2,17 @@
 
 set -eu
 
+REPO=${REPO:-repo}
+BINARY=risq
+
 export CARGO_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../" >/dev/null && pwd )/cargo-home"
 
-pushd repo
+pushd ${REPO}
 
 export CARGO_TARGET_DIR="../cargo-target-dir"
 
 make build-${TARGET}-release
+
+if [[ "${OUT}" != "" ]]; then
+mv ${CARGO_TARGET_DIR}/${TARGET}/release/${BINARY} ${RELEASE_ROOT}/artifacts/${BINARY}-${TARGET}
+fi
