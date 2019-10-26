f87630b06360cd54835fd624c855772297863cdc
Justin Carter <justin@misthos.io>
2019-10-26 14:57:13 +0200

    Don't cache CARGO_HOME in verify step

--- ci/tasks/verify-publish.sh
+++ ci/tasks/verify-publish.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export CARGO_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../" >/dev/null && pwd )/cargo-home"
-
 cargo install --all-features risq
 if [[ $(risq --version) != "risq $(cat version/number)" ]]; then
   echo "Installed risq does not have expected version number"
